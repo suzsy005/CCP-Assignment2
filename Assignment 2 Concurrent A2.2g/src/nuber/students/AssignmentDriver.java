cf407e73adf7364cd9d502d0612977dd0b6cea10
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-06 16:11:59 +1030

    Checked all the Simulation() worked!!!
    I'll see if all of them will terminate after running

--- Assignment 2 Concurrent A2.2g/src/nuber/students/AssignmentDriver.java
+++ Assignment 2 Concurrent A2.2g/src/nuber/students/AssignmentDriver.java
@@ -67,11 +67,11 @@ public class AssignmentDriver {
 		// couldn't figure out what is wrong and what to fix
 		// I'll check this our tomorrow
 		
-		//new Simulation(regions, 1, 10, 1000, logEvents);
-		//new Simulation(regions, 5, 10, 1000, logEvents);
-		//new Simulation(regions, 10, 10, 1000, logEvents);
-		//new Simulation(regions, 10, 100, 1000, logEvents);
-		//new Simulation(regions, 1, 50, 1000, logEvents);
+//		new Simulation(regions, 1, 10, 1000, logEvents);
+//		new Simulation(regions, 5, 10, 1000, logEvents);
+//		new Simulation(regions, 10, 10, 1000, logEvents);
+//		new Simulation(regions, 10, 100, 1000, logEvents);
+//		new Simulation(regions, 1, 50, 1000, logEvents);
 	}
 
 }
